b15f4476350c8d95efa320b3ea062e5d02a13683
dMosca <devinmosca@yahoo.com>
2021-09-10 14:41:04 -0600

    Started psuedocode

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,12 @@ CREATE DATABASE workforce_db;
 
 USE workforce_db;
 
-CREATE TABLE students (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  enrolled BOOLEAN NOT NULL
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  title_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  salary INT NOT NULL,
+  employment_status BOOLEAN NOT NULL
 );
---
+++ server.js
@@ -0,0 +1,50 @@
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const util = require('util');
+
+// // create writeFile function using promises instead of a callback function
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+// const promptUser = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'location',
+//       message: 'Where are you from?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'hobby',
+//       message: 'What is your favorite hobby?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'food',
+//       message: 'What is your favorite food?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'github',
+//       message: 'Enter your GitHub Username',
+//     },
+//     {
+//       type: 'input',
+//       name: 'linkedin',
+//       message: 'Enter your LinkedIn URL.',
+//     },
+//   ]);
+// };
+
+// Template for inquirer, need to use prompts to:
+    // View all employees <--table
+    // Add employee <--seed?
+    // Update employee role 
+    // View all roles <--table
+    // Add role <--seed?
+    // View all departments <--table
+    // Add department <--seed?
