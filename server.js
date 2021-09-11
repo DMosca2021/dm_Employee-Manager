6141612924785c2f455f91b06b5e20d4e03e3d6e
dMosca <devinmosca@yahoo.com>
2021-09-11 09:16:52 -0600

    Moved file into proper location

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const inquirer = require('inquirer');
+const path = require("path");
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
+// /api/notes/
+
+// // GET Route for homepage
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, "./public/index.html"))
+// );
+
+// // GET Route for notes page
+// app.get('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/notes.html'))
+// );
+
+// // GET Route for catch all
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, './public/index.html'))
+// );
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+// Borrowed server format from last homework
